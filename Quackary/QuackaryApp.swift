727b7fc17e01217d55ea99b9a11923fe142d75e4
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2024-06-21 13:40:38 +0700

    feat: try GachaView on Main

--- Quackary/QuackaryApp.swift
+++ Quackary/QuackaryApp.swift
@@ -26,7 +26,7 @@ struct QuackaryApp: App {
 
     var body: some Scene {
         WindowGroup {
-            AchievementView()
+            GachaView()
         }
         .modelContainer(sharedModelContainer)
     }
