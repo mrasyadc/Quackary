e0be12299103ae1f91f578ad0ffda2a26235b403
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2024-06-23 07:04:50 +0700

    fix: remove AchievementViewModel

--- Quackary/ViewModel/AchievementViewModel.swift
+++ Quackary/ViewModel/AchievementViewModel.swift
@@ -8,22 +8,4 @@
 import Foundation
 import SwiftUI
 
-class AchievementViewModel: ObservableObject {
-    init() {}
-
-    func getAllAchievement() {
-        // Example usage:
-        let achievementManager = Achievements()
-
-        achievementManager.addAchievement(name: "First Steps", detail: "Complete the first task", imageName: "first_steps.png")
-        achievementManager.addAchievement(name: "Master", detail: "Complete all tasks", imageName: "master.png")
-
-        if let firstAchievement = achievementManager.findAchievement(by: UUID()) {
-            achievementManager.setAchievementToFinish(achievementId: firstAchievement.achievementId)
-        }
-
-        // Get all finished achievements
-        let finishedAchievements = achievementManager.getFinishedAchievements()
-        print(finishedAchievements.map { $0.name }) // Output depends o
-    }
-}
+class AchievementViewModel: ObservableObject {}
