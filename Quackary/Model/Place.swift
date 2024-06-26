ffdd0102ceeeca10cf81718820add1d89a3804cf
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2024-06-26 11:34:41 +0700

    chore: cleanup

--- Quackary/Model/Place.swift
+++ Quackary/Model/Place.swift
@@ -68,19 +68,3 @@ final class Place {
         isRestrict = true
     }
 }
-
-// Example usage:
-// let placesManager = Places()
-//
-// placesManager.addPlace(name: "Cafe Delight", lat: 40.748817, long: -73.985428, priceRange: 2, openingHour: "08:00", closingHour: "22:00", imageNames: ["cafe1.png", "cafe2.png"], foods: [.Chicken, .Salad])
-// placesManager.addPlace(name: "Bistro Joy", lat: 40.748817, long: -73.985428, priceRange: 3, openingHour: "10:00", closingHour: "20:00", imageNames: ["bistro1.png", "bistro2.png"], foods: [.Fish, .Pasta])
-//
-// if let cafeDelight = placesManager.findPlace(by: UUID()) {
-//    placesManager.setPlaceToFav(placeId: cafeDelight.placeId)
-// }
-//
-// let favoritePlaces = placesManager.getFavoritePlaces()
-// print(favoritePlaces.map { $0.name }) // Output depends on which places are marked as favorite
-//
-// let openPlaces = placesManager.getOpenPlaces()
-// print(openPlaces.map { $0.name }) // Output depends on the current time and places' opening hours
