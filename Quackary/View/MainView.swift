9266720a58f9d543082400c608d271a9fc675157
Ragnarom-kun <romi.fadhurrohman@gmail.com>
2024-06-26 09:26:32 +0700

    feat: add router to RestrictFoodView and fix styling RestrictFoodView

--- Quackary/View/MainView.swift
+++ Quackary/View/MainView.swift
@@ -53,6 +53,8 @@ struct MainView: View {
                         RewardsClaimView()
                     case .WorldViewUpdated:
                         WorldUpdatedView()
+                    case .RestrictFoodView:
+                        RestrictFoodView()
                     }
                 }
         }
--- Quackary/View/RestrictFoodView.swift
+++ Quackary/View/RestrictFoodView.swift
@@ -18,45 +18,82 @@ struct RestrictFoodView: View {
     var body: some View {
         VStack(alignment: .leading) {
             
-            Text("Your Food Restrictions")
-                .font(Font.custom("Lato-ExtraBold", size: 22))
-                .foregroundColor(.blueDarker)
-            Text("Tell us what’s a no for you!")
-                .font(Font.custom("Lato-Regular", size: 16))
-                .foregroundColor(.blueDarker)
+            HStack(spacing: 12) {
+                VStack(alignment: .leading, spacing: 10) {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 107, height: 4)
+                        .background(Color(red: 0.27, green: 0.48, blue: 0.62))
+                        .cornerRadius(100)
+                }
+                .cornerRadius(10)
+                VStack(alignment: .leading, spacing: 10) {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 107, height: 4)
+                        .background(Color(red: 0.93, green: 0.94, blue: 0.93))
+                        .cornerRadius(100)
+                }
+                .cornerRadius(10)
+                VStack(alignment: .leading, spacing: 10) {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 107, height: 4)
+                        .background(Color(red: 0.93, green: 0.94, blue: 0.93))
+                        .cornerRadius(100)
+                }
+                .cornerRadius(10)
+            }
+            .padding(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
+            .frame(width: 393, height: 44)
+            
+            VStack(alignment: .leading) {
+                Text("Your Food Restrictions")
+                    .font(Font.custom("Lato-ExtraBold", size: 22))
+                    .foregroundColor(.blueDarker)
+                Text("Tell us what’s a no for you!")
+                    .font(Font.custom("Lato-Regular", size: 16))
+                    .foregroundColor(.blueDarker)
+            }.padding()
             
             let columns = [
                 GridItem(.flexible(minimum: 70)),
                 GridItem(.flexible(minimum: 70)),
                 GridItem(.flexible(minimum: 70)),
                 GridItem(.flexible(minimum: 70)),
-                GridItem(.flexible(minimum: 70))
+                GridItem(.flexible(minimum: 70)),
             ]
             
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(RestrictType.allCases, id: \.self) { restriction in
-                    Button(action: {
-                        if selectedRestrictions.contains(restriction) {
-                            selectedRestrictions.remove(restriction)
-                        } else {
-                            selectedRestrictions.insert(restriction)
+            HStack(alignment: .top, spacing: 12) {
+                LazyVGrid(columns: columns, spacing: 12) {
+                    ForEach(RestrictType.allCases, id: \.self) { restriction in
+                        Button(action: {
+                            if selectedRestrictions.contains(restriction) {
+                                selectedRestrictions.remove(restriction)
+                            } else {
+                                selectedRestrictions.insert(restriction)
+                            }
+                        }) {
+                            Text(restriction.rawValue)
+                                .padding(.vertical, 30)
+                                .frame(maxWidth: .infinity)
+                                .background(selectedRestrictions.contains(restriction) ? Color.orange : Color.gray.opacity(0.2))
+                                .cornerRadius(21)
+                                .foregroundColor(.black)
                         }
-                    }) {
-                        Text(restriction.rawValue)
-                            .padding(.vertical, 30)
-                            .frame(maxWidth: .infinity)
-                            .background(selectedRestrictions.contains(restriction) ? Color.orange : Color.gray.opacity(0.2))
-                            .cornerRadius(21)
-                            .foregroundColor(.black)
                     }
                 }
+                .padding()
             }
-            .padding()
+            .padding(0)
+            .frame(width: 353, alignment: .topLeading)
+            
             Spacer()
             
             HStack {
                 Button(action: {
                     // Handle previous action here
+                    Router.shared.path.popLast()
                 }) {
                     Image("Back_Button")
                         .resizable()
@@ -67,6 +104,7 @@ struct RestrictFoodView: View {
                 if selectedRestrictions.count >= 3 {
                     Button(action: {
                         // Handle the next action here
+//                        Router.shared.path.append(<#T##Element#>)
                     }) {
                         Image("Next_Button")
                             .resizable()
